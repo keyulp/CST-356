d7814092ee379fc457bcf853ed8023fa103dd08c
keyulp <keyulp9713406713@gmail.com>
2018-01-15 19:57:22 -0800

    Lab 1

---
+++ Lab 1/Lab 1/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This is the program for Web Design and Development.";
+            ViewBag.Message2 = "My Name is Keyul Patel and I am a CSET Student at Oregon Institute of Technology.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }   
+    }
+}
---
+++ Lab 1/Lab 1/Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab_1.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Person()
+        {
+            ViewBag.NewPerson = "This is a new person.";
+            ViewBag.PersonName = "His name is Skylar.";
+            ViewBag.PersonJob = "He works as a trainer at the local gym";
+            return View();
+        }
+    }
+}
