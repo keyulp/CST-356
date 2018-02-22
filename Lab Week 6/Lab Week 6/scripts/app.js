5d498fde22add5dd9fda1c3fb7b988e367db79b5
keyulp <keyulp9713406713@gmail.com>
2018-02-21 17:58:12 -0800

    Done

---
+++ Lab Week 6/Lab Week 6/scripts/app.js
@@ -0,0 +1,26 @@
+﻿function displaySummary() {
+
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var age = document.getElementById("age").value;
+
+    if (firstName == null || firstName == "") {
+        alert("Please Fill In First Name Field");
+        return false;
+    }
+    else if (lastName == null || lastName == "") {
+        alert("Please Fill In Last Name Field");
+        return false;
+    }
+    else if (age == null || age == "") {
+        alert("Please Fill In Age Field")
+    }
+    else {
+
+        var summary = firstName + " " + lastName + " is " + age + " years old.";
+
+        document.getElementById("summary").innerText = summary;
+
+        return false;
+    }
+}
