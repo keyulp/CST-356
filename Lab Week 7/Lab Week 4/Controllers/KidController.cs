77bfc2f2eb8056d5dcdc812fe20e4bb3ea166c46
keyulp <keyulp9713406713@gmail.com>
2018-02-24 20:29:11 -0800

    Done

---
+++ Lab Week 7/Lab Week 4/Controllers/KidController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Lab_Week_4.Data;
+using Lab_Week_4.Data.Entities;
+using Lab_Week_4.Models.View;
+
+
+namespace Lab_Week_4.Controllers
+{
+    public class KidController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create(int userId)
+        {
+            ViewBag.UserId = userId;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(KidViewModel kidViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var context = new DatabaseContext();
+
+                var kid = MapToKid(kidViewModel);
+
+                context.Kids.Add(kid);
+
+                context.SaveChanges();
+
+                return RedirectToAction("List", new { UserId = kidViewModel.UserId });
+            }
+
+            return View();
+        }
+
+        public ActionResult List(int userId)
+        {
+            ViewBag.UserId = userId;
+
+            var kidViewModels = new List<KidViewModel>();
+
+            var context = new DatabaseContext();
+
+            var kidList = context.Kids.Where(pet => pet.UserId == userId).ToList();
+
+            foreach (var kid in kidList)
+            {
+                var kidViewModel = MapToKidViewModel(kid);
+                kidViewModels.Add(kidViewModel);
+            }
+
+            return View(kidViewModels);
+        }
+
+        private Kid MapToKid(KidViewModel kidViewModel)
+        {
+            return new Kid
+            {
+                Id = kidViewModel.Id,
+                FirstName = kidViewModel.FirstName,
+                LastName = kidViewModel.LastName,
+                Age = kidViewModel.Age,
+                Parent1 = kidViewModel.Parent1,
+                Parent2 = kidViewModel.Parent2,
+                Birthday = kidViewModel.Birthday,
+                UserId = kidViewModel.UserId
+            };
+        }
+
+        private KidViewModel MapToKidViewModel(Kid kid)
+        {
+            return new KidViewModel
+            {
+                Id = kid.Id,
+                FirstName = kid.FirstName,
+                LastName = kid.LastName,
+                Age = kid.Age,
+                Parent1 = kid.Parent1,
+                Parent2 = kid.Parent2,
+                Birthday = kid.Birthday,               
+                UserId = kid.UserId
+            };
+        }
+    }
+}
---
+++ Lab Week 7/Lab Week 4/Models/View/UserViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab_Week_4.Models.View
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [Display(Name = "DOB")]
+        public DateTime DOB { get; set; }
+
+        [Required]
+        [Display(Name = "Years in School")]
+        public int YearsInSchool { get; set; }
+    }
+
+
+}
